76c25fdf58e4c0072b9b3c9d95be37a4538ff17f
Barbara Dainese <barbara.dainese@venturus.org.br>
2022-02-22 13:19:36 -0300

    atualizando js

--- functions/functions2.js
+++ functions/functions2.js
@@ -107,4 +107,54 @@ console.log(paulo.walk())
      console.log(Number ('9')+ 5) // conversão da string para number -> saída = 14
 
 
+//Transformar String em um número e número em string
+
+    let string = "123"
+     console.log(Number(string))
+
+     let number = 321
+    console.log(String(number))
+
+// Contar quantos caracteres tem uma palavra e quantos digitos tem um número
+
+    let word = "paralelepipedo"
+    console.log(word.length)
+
+    let numberb = 1234
+    console.log(String(numberb).length)
+
+// Transformar um número quebrado com 2 casas decimais e trocar ponto por virgula
+
+    let numberc = 34.0989678
+    console.log(numberc.toFixed(2).replace(".", ","))
+
+
+// Transformar letras maiúsculas em minúsculas e o contrrário também:
+    
+let phraseone = "programar é melhor que lazzagna"
+console.log(phraseone.toUpperCase())
+
+
+// Separe um texto que contém espaços, em um novo array onde cada texto é uma posição do array. Depois disso, transforme o array em um texto e onde eram espaços coloque _
+
+let text = "Quero uma casa na praia!"
+let myArray = text.split(" ")
+console.log(text)
+console.log(myArray )
+let textWithUnderscore = myArray.join("_")
+console.log(textWithUnderscore)
+
+//Verificar de o texto contém a palavra Praia
+
+console.log(text.includes("praia"))
+
+
+
+
+
+
+
+
+
+
 
