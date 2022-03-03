d6084ef9d1ef5f11da3e21432aaafda76ebc9f24
Barbara Dainese <barbara.dainese@venturus.org.br>
2022-03-03 09:11:06 -0300

    nova pasta

---
+++ op.js
@@ -0,0 +1,27 @@
+/*
+
+    Expressões e operadores
+
+    - Expressions
+    - Operators
+        Binary
+        Unary
+        Ternary
+
+*/
+
+let number = 1
+
+console.log(number + 1)
+
+console.log (++number)
+
+console.log(false ? "alo" : "nada")
+
+/*
+    new
+
+    * left-hand-side expression
+    * criar um novo objeto
+
+*/
