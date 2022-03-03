d0f0e0b8fac56783e209977e81d9e9d55010e73d
Barbara Dainese <barbara.dainese@venturus.org.br>
2022-03-03 11:04:34 -0300

    commit on main

--- op.js
+++ op.js
@@ -25,3 +25,60 @@ console.log(false ? "alo" : "nada")
     * criar um novo objeto
 
 */
+
+let nome = new String('Barbara')
+nome.surName = "Dainese"
+let age = new Number(35)
+
+console.log(nome, age)
+
+let date = new Date('2022-03-03')
+console.log(date)
+
+/*
+
+    Operadores unários
+
+    typeof
+    delete
+
+*/
+
+console.log(typeof"barbara")
+
+const person={
+    name: 'Barbara',
+    age:35,
+}
+ 
+delete person.age
+  
+console.log(person)
+
+//Operadores aritiméticos
+
+//miltiplicação *
+console.log(3*5)
+
+//divisão
+console.log(10/2)
+
+//soma
+console.log(2+3)
+
+//subtração
+console.log(3-2)
+
+//resto da divisão
+console.log(11%3)
+
+//incremento
+let num = 0
+console.log(++num)
+
+//decremento
+let numb=3
+console.log(--numb)
+
+//exponencial 
+console.log(3**2)
